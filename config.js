a9368912e0de502e8d8123bc435c38c19ebc74b4
FriendsIndonesia <friendsindonesia28@gmail.com>
2026-08-20 11:21:12 +0700

    Connect Apps Script backend

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.HABIT_TRACKER_CONFIG = {
   backendProvider: "google-workspace",
   workspaceAccount: "friendsindonesia28@gmail.com",
-  googleAppsScriptUrl: "",
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbz726sxlw16o7jCF7eq8HcAkK-Zp5EgXo3C5Wifk58EOQeoM09Za-T4oHrv0d-qnFA/exec",
   githubPages: {
     enabled: true,
     branch: "main",
