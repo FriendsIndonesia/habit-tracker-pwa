be3a572409869e591e25cf4e3833e24e9c7a2af8
FriendsIndonesia <friendsindonesia28@gmail.com>
2026-08-20 09:19:32 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.HABIT_TRACKER_CONFIG = {
+  backendProvider: "google-workspace",
+  workspaceAccount: "friendsindonesia28@gmail.com",
+  googleAppsScriptUrl: "",
+  githubPages: {
+    enabled: true,
+    branch: "main",
+    folder: "/"
+  }
+};
