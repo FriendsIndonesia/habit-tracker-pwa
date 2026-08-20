8ee93b7a23e63852a90b42fed082be13b76b5758
FriendsIndonesia <friendsindonesia28@gmail.com>
2026-08-20 09:21:53 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "habit-tracker-phase1-v5-network-first";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./config.js",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+      )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
